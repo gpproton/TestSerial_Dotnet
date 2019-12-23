0f1010ae298c8e26561de9cc5fa0ec77a8c291af
Godwin peter .O <me@godwin.dev>
2019-12-23 09:51:35 +0100

    Complete test experiments.

--- TestSerial_Dotnet/Program.cs
+++ TestSerial_Dotnet/Program.cs
@@ -2,7 +2,11 @@
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Numerics;
 using System.Threading.Tasks;
+using NLog;
+using TestSerial_Dotnet.model;
 
 namespace TestSerial_Dotnet
 {
@@ -10,8 +14,39 @@ namespace TestSerial_Dotnet
     class Program
     {
         static int _Fixed_Bytes = 20;
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+
+            string dbName = "TestDatabase.db";
+            if (File.Exists(dbName))
+            {
+                File.Delete(dbName);
+            }
+
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("file") { FileName = "testinfo.log" };
+            var logconsole = new NLog.Targets.ConsoleTarget("console");
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+
+
+            //Check if Internet is ON or OF
+            if(HasConnection())
+            {
+                Console.WriteLine("Internet ON");
+            }
+            else
+            {
+                Console.WriteLine("Internet OFF");
+            }
+
             string[] ports = SerialPort.GetPortNames();
 
             Console.WriteLine("Serial ports available:");
@@ -21,14 +56,19 @@ namespace TestSerial_Dotnet
             {
                 Console.WriteLine(port);
             }
+            Console.WriteLine("-----------------------");
 
             // SerialPort srp = new SerialPort("/dev/tty.usbserial-1410", 9600);
-            SerialPort srp = new SerialPort {BaudRate = 9600, PortName = "/dev/tty.usbserial-1410", DataBits = 8};
+            String serialName = "/dev/tty.usbserial-1410";
+            SerialPort srp = new SerialPort {BaudRate = 9600, PortName = serialName, DataBits = 8};
 
 
             int CountBits = 0;
             byte[] tempByte = new byte[13];
-            
+            Random rnd = new Random();
+            var dbContext = new AppsContext();
+            dbContext.Database.EnsureCreated();
+
             try
             {
                 srp.Open();
@@ -42,22 +82,60 @@ namespace TestSerial_Dotnet
                     
                     if (srp.BytesToRead > 0)
                     {
-                        
                         byte b = (byte)srp.ReadByte();
                         if (Enumerable.Range(7,18).Contains(CountBits)) {
                             tempByte[CountBits - 7] = b;
                         }
+                        var finalHex = BitConverter.ToString(tempByte).Replace("-", string.Empty);
+                        var cutHex = finalHex.Replace("E20000", string.Empty);
+
+                        //var cardNumber = Convert.ToUInt64("16370402410910C2E9", 16);
+                        var cardNumber = BigInteger.Parse(cutHex, System.Globalization.NumberStyles.HexNumber);
+
                         if (CountBits == _Fixed_Bytes -1) {
-                            Console.WriteLine(BitConverter.ToString(tempByte));
-                        // foreach(byte xx in tempByte) {
-                        //     Console.WriteLine(xx);
-                        // }
+
+                            //Ensure database is created
+                            //if (!dbContext.Cards.Any())
+                            //{
+                            
+                            //}
+
+                            try
+                            {
+                                dbContext.Cards.AddRange(new Card[]
+                                {
+                                            new Card{ Id=rnd.Next(999999999), CardNo=(decimal)cardNumber, Location=1, SubLocation=23, CaptureType=1 }
+                                });
+
+                                if (dbContext.SaveChanges() > 0)
+                                {
+                                    try
+                                    {
+                                        Logger.Info("UHF RFID NO:- " + cardNumber);
+
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Logger.Error(ex, "Goodbye cruel errors...");
+                                    }
+                                }
+
+                                
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                            }
+                            // foreach(byte xx in tempByte) {
+                            //     Console.WriteLine(xx);
+                            // }
                         }
                         // Console.WriteLine(b + "----" + CountBits.ToString());
 
                         CountBits++;
                     }
-                }
+                    
+    }
             }
             catch (IOException e)
             {
@@ -66,5 +144,25 @@ namespace TestSerial_Dotnet
 
             // Console.ReadLine();
         }
+
+        //Check if Internet is ON or OF
+        public static bool HasConnection()
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "google.com";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                return (reply.Status == IPStatus.Success);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
+
 }
---
+++ TestSerial_Dotnet/model/AppsContext.cs
@@ -0,0 +1,92 @@
+﻿//
+// AppsContext.cs
+//
+// Author:
+//       Godwin peter .O <me@godwin.dev>
+//
+// Copyright (c) 2019 MIT 2019
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestSerial_Dotnet.model
+{
+    public class AppsContext : DbContext
+    {
+        private readonly string connectionString;
+
+        public AppsContext()
+        {
+        }
+
+        public AppsContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DbSet<Card> Cards { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            _ = optionsBuilder.UseSqlite("Data Source=TestDatabase.db;", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map table names
+            modelBuilder.Entity<Card>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasIndex(e => e.Id).IsUnique();
+                entity.Property(e => e.CaptureTime).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public interface ICardProvider
+    {
+        Card Get(int Id);
+    }
+
+    public class CardProvider : ICardProvider
+    {
+        readonly AppsContext AppsContext;
+
+        CardProvider(AppsContext AppsContext)
+        {
+            this.AppsContext = AppsContext;
+        }
+
+        public Card Get(int Id)
+        {
+            return AppsContext.Cards.Where(e => e.Id == Id).FirstOrDefault();
+        }
+    }
+
+}
